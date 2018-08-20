b6fbc5705096befd27b641f6f58cd6b8227dfd3e
d9sskssssjss <42514461+wonga20082018@users.noreply.github.com>
2018-08-20 14:29:54 +0200

    Add files via upload

---
+++ ConsoleCoreReceiver/ConsoleCoreReceiver/UnitTestCoreReceiver/UnitTestReceiver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleCoreReceiver;
+
+namespace UnitTestCoreReceiver
+{
+    [TestClass]
+    public class UnitTestReceiver
+    {
+        [TestMethod]
+        public void DisplaySentMessage()
+        {
+            string msg = "Hello my name is, Kat";
+            string expected = "Hello Kat , I am your father!";
+
+            Assert.AreEqual(expected, Prompt.ReturnPrompt(msg));
+        }
+    }
+}
+
