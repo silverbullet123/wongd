3fc63475b7975021804791b632566bcbc8a6db33
d9sskssssjss <42514461+wonga20082018@users.noreply.github.com>
2018-08-20 14:28:05 +0200

    Add files via upload

---
+++ ConsoleCoreReceiver/ConsoleCoreReceiver/Prompt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCoreReceiver
+{
+    public static class Prompt
+    {
+        public static string ReturnPrompt(string msg)
+        {
+            int periodIndex = msg.IndexOf(',');
+            periodIndex = periodIndex + 1;
+            msg = msg.Substring(periodIndex);
+            return "Hello " + msg.Trim() + " , I am your father!";
+        }
+    
+    }
+}
---
+++ ConsoleCoreReceiver/ConsoleCoreReceiver/RabbitReceiver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ConsoleCoreReceiver
+{
+    class RabbitReceiver
+    {
+
+        private IConnection connection;
+        private IModel channel;
+
+        public void Connect()
+        {
+            try
+            {
+                ConnectionFactory factory = new ConnectionFactory
+                {
+                    HostName = ConnectionConstants.HostName
+                };
+
+                connection = factory.CreateConnection();
+                channel = connection.CreateModel();
+                channel.QueueDeclare(queue: ConnectionConstants.QueueName,
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Consume()
+        {
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(Prompt.ReturnPrompt(message));
+            };
+            channel.BasicConsume(queue: ConnectionConstants.QueueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+        }
+    }
+}
